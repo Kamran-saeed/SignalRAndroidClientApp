7d57459129a3d6ed29e02a69ae4485ec149cb300
Kamran-saeed <kamransaeed1000@gmail.com>
2017-09-13 01:15:08 +0500

    first change

---
+++ chatApp/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(chatApp.Startup))]
+
+namespace chatApp
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
